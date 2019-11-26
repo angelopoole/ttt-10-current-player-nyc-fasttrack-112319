a2a5d09511cef7d3e39f4a2153eb99b526b37a5b
Angelo Poole <angelo.poole@outlook.com>
2019-11-26 12:35:04 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+#two methods, one for how many turns played
+#one based on information, an x if its is player x's turna nd an o id its player o's turn
+counter = 0
+def turn_count(board)
+  board.each do |count|
+    if count != "" || " "
+      counter +=
+    end
+end
+counter
+def current_player(board)
+end
